7223c302c5af12c412993f0e9f5f118898a58cbc
Nathan <krulic1325@gmail.com>
2021-04-05 12:12:19 +0200

    ajout bottom navigation menu + fragments pour chaques fenetres ( signaler , map , itineraire )

--- app/src/main/java/com/moustache/professeur/balancetondechet/view/MainMenuActivity.java
+++ app/src/main/java/com/moustache/professeur/balancetondechet/view/MainMenuActivity.java
@@ -3,44 +3,54 @@ package com.moustache.professeur.balancetondechet.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.moustache.professeur.balancetondechet.R;
 import com.moustache.professeur.balancetondechet.model.User;
 
-public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainMenuActivity extends AppCompatActivity {
     private User currentUser;
-    private TextView currentUserTextView;
-    private Button osmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
-        currentUserTextView = (TextView) findViewById(R.id.currentUserTextView);
         currentUser = getIntent().getExtras().getParcelable("user");
-        currentUserTextView.setText(currentUser.getNom());
 
-        osmButton = (Button) findViewById(R.id.OsmButton);
-        osmButton.setOnClickListener(this);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new MapFragment()).commit();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.OsmButton:
-                Log.v("ButtonClick", "Click on osm");
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
 
-                Intent osmIntent = new Intent(this, OsmActivity.class);
-                startActivity(osmIntent);
-                break;
-            default:
-                Log.v("ButtonClick", "Unknown button click");
-                break;
+            switch (item.getItemId()){
+                case R.id.nav_carte:
+                    selectedFragment = new MapFragment();
+                    break;
+                case R.id.nav_itineraire:
+                    selectedFragment = new ItineraireFragment();
+                    break;
+                case R.id.nav_signaler :
+                    selectedFragment = new SignalerFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
         }
-    }
+    };
+
 }
