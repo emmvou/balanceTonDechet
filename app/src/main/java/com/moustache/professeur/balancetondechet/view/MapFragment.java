ade03ce17c892fe42731b8283c1561199ae6c383
Léo Pourcelot <leo.pourcelot@etu.univ-cotedazur.fr>
2021-04-06 18:53:40 +0200

    Ajout du code faisant fonctionner la carte

--- app/src/main/java/com/moustache/professeur/balancetondechet/view/MapFragment.java
+++ app/src/main/java/com/moustache/professeur/balancetondechet/view/MapFragment.java
@@ -8,14 +8,57 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import com.moustache.professeur.balancetondechet.R;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 public class MapFragment extends Fragment {
+    private MapView map;
+    private IMapController mapController;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map,container,false);
+        View view = inflater.inflate(R.layout.fragment_map,container,false);
+
+        Configuration.getInstance().load(
+                view.getContext(),
+                PreferenceManager.getDefaultSharedPreferences(view.getContext())
+        );
+
+        map = view.findViewById(R.id.map);
+
+        initializeMapIfNeeded();
+
+        return view;
+    }
+
+    private void initializeMapIfNeeded() {
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        // TODO: use non-deprecated code instead
+        map.setBuiltInZoomControls(true);
+        // TODO: use GPS-provided coordinates instead
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+        mapController = map.getController();
+        mapController.setCenter(startPoint);
+        mapController.setZoom(18.0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
     }
 }
