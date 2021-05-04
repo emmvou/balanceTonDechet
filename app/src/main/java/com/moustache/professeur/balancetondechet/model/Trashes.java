96d4d71f36610457a0762cbb528098121f283e39
Léo Pourcelot <leo.pourcelot@etu.univ-cotedazur.fr>
2021-05-04 16:23:29 +0200

    Add more and more hardcoded data

--- app/src/main/java/com/moustache/professeur/balancetondechet/model/Trashes.java
+++ app/src/main/java/com/moustache/professeur/balancetondechet/model/Trashes.java
@@ -20,13 +20,6 @@ public class Trashes {
     public Trashes(){
         super();
         Log.v("Trashes","Construction de la liste de déchet...");
-        trashes.add(new Trash("TV cathodique","TV cathodique avec écran brisé en morceau au 8 rue de la Chappelle",new TrashPin(43.6173,7.0747), false, true,"test@gmail.com","/storage/emulated/0/DCIM/Camera/IMG_20210503_132915.jpg",Type.ENCOMBRANT));
-        trashes.add(new Trash("Meuble en bois","Meuble en bois au bord de la route mirabeau",new TrashPin(43.6168,7.0743), false, true, "test@gmail.com","/storage/emulated/0/DCIM/Camera/IMG_20210503_132915.jpg",Type.ENCOMBRANT));
-        trashes.add(new Trash("Micro-onde", "Micro-onde encore utilisable", new TrashPin(43.6173, 7.0724), "test@gmail.com", "", Type.PORTABLE));
-        trashes.add(new Trash("Carcasse de voiture", "Carosserie de fiat 500 calcinée et rouillée", new TrashPin(43.6161, 7.0732), false, true, "test@gmail.com", "", Type.TRES_ENCOMBRANT));
-        trashes.add(new Trash("Tessons de bouteille", "Tessons de plusieurs bouteilles de bière", new TrashPin(43.6173, 7.0760), false, true, "test@gmail.com", "", Type.PORTABLE));
-        trashes.add(new Trash("Vieil écran HS", "Écran inutilisé depuis des lustres, mis au rebus par les laboratoires", new TrashPin(43.6167, 7.0710), false, true, "test@gmail.com", "", Type.ENCOMBRANT));
-        trashes.add(new Trash("Canapé vétuste", "Vieux canapé des années 50", new TrashPin(43.6164, 7.0782), false, true, "test@gmail.com", "", Type.TRES_ENCOMBRANT));
     }
 
     public void add (Trash t){
