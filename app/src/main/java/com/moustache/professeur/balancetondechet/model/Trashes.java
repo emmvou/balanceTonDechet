1547a8e3502252be9228bc8bc62e4114739d993c
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2021-05-04 14:54:06 +0200

    trash now addable via code but path all bugged

--- app/src/main/java/com/moustache/professeur/balancetondechet/model/Trashes.java
+++ app/src/main/java/com/moustache/professeur/balancetondechet/model/Trashes.java
@@ -20,8 +20,8 @@ public class Trashes {
     public Trashes(){
         super();
         Log.v("Trashes","Construction de la liste de déchet...");
-        trashes.add(new Trash("TV cathodique","TV cathodique avec écran brisé en morceau au 8 rue de la Chappelle",new TrashPin(43.6173,7.0747),"test@gmail.com","/storage/emulated/0/DCIM/Camera/IMG_20210503_132915.jpg",Type.ENCOMBRANT));
-        trashes.add(new Trash("Meuble en bois","Meuble en bois au bord de la route mirabeau",new TrashPin(43.6168,7.0743),"test@gmail.com","/storage/emulated/0/DCIM/Camera/IMG_20210503_132915.jpg",Type.ENCOMBRANT));
+        trashes.add(new Trash("TV cathodique","TV cathodique avec écran brisé en morceau au 8 rue de la Chappelle",new TrashPin(43.6173,7.0747), false, true, "test@gmail.com","/storage/emulated/0/DCIM/Camera/IMG_20210503_132915.jpg",Type.ENCOMBRANT));
+        trashes.add(new Trash("Meuble en bois","Meuble en bois au bord de la route mirabeau",new TrashPin(43.6168,7.0743), false, true, "test@gmail.com","/storage/emulated/0/DCIM/Camera/IMG_20210503_132915.jpg",Type.ENCOMBRANT));
     }
 
     public void add (Trash t){
